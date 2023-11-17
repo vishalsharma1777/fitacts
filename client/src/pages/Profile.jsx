42a0195ec1a40010896fe9ed253c825a64085a28
Vishal Sharma <vishal1777.be20@chitkara.edu.in>
2023-11-17 10:36:51 +0530

    added feedback

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,6 +1,8 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, forwardRef } from 'react';
 import Navbar from '../components/Common/Navbar';
 import Favicon from 'react-favicon';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
 import { jwtDecode } from 'jwt-decode';
 import { getUserById } from '../apis/userapis';
 import Card from '@mui/material/Card';
@@ -18,10 +20,19 @@ import { tableDataActions } from '../store/TableDataSlice';
 import { timelineActions } from '../store/timelineSlice';
 import { userActions } from '../store/userSlice';
 
+const Alert = forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant='filled' {...props} />;
+});
+
 function Profile() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const [user, setUser] = useState({});
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [openSnackBar, setOpenSnackBar] = useState(false);
+  const [severity, setSeverity] = useState('success');
+
   if (localStorage.getItem('token') == null) {
     return <StartingPage />;
   }
@@ -32,8 +43,12 @@ function Profile() {
         setUser(res.data);
       })
       .catch((err) => {
+        setError(true);
+        setOpenSnackBar(true);
         console.log(err);
-        console.log(err);
+        {err.message && setErrorMessage(err.message)}
+        setErrorMessage(err.response.data.message)
+        setSeverity('error')
       });
     document.title = 'Fit Acts | Profile';
   }, []);
@@ -56,12 +71,40 @@ function Profile() {
     localStorage.removeItem('ClickedUser');
     localStorage.removeItem('ClickedId');
     navigate('/');
-  }
+  };
+
+  const handleClickSnackBar = () => {
+    setOpenSnackBar(true);
+  };
+
+  const handleCloseSnackBar = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpenSnackBar(false);
+  };
+
 
   return (
     <>
       <Favicon url='https://as2.ftcdn.net/v2/jpg/03/17/39/23/1000_F_317392365_DbNbOQyBnMmn1ahAGc3cdotSXYyI7BoA.jpg'></Favicon>
       <Navbar />
+      {error && (
+        <Snackbar
+          anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+          open={openSnackBar}
+          autoHideDuration={3000}
+          onClose={handleCloseSnackBar}
+        >
+          <Alert
+            onClose={handleCloseSnackBar}
+            severity={severity}
+            sx={{ width: '100%' }}
+          >
+            {errorMessage}
+          </Alert>
+        </Snackbar>
+      )}
       <div className='details-center'>
         <Card sx={{ maxWidth: 400 }}>
           <CardContent>
@@ -91,13 +134,15 @@ function Profile() {
             </p>
             <p className='detail'>
               {' '}
-              <Button variant='contained' color='error' sx={
-                {
+              <Button
+                variant='contained'
+                color='error'
+                sx={{
                   width: '100%',
                   height: '100%'
-                }
-              }
-              onClick={handleLogout}>
+                }}
+                onClick={handleLogout}
+              >
                 LOGOUT
               </Button>
             </p>
